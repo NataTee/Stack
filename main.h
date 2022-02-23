8047b835b37206d6b3f735549413a4664ace1407
Natalia Ksenz <velvet@MacBook-Pro.local>
2022-02-22 23:11:41 -0800

    included stack.h and defined Multiplier

--- main.h
+++ main.h
@@ -1,23 +1,24 @@
-/*
- * your header here
- * 
- */
+/***************************************************************************
+Name: Natalia Ksenz
+Coding 04
+Purpose: The assignment to create a stack object that will work with a struct
+data type called Data.
+
+File: main.h
+***************************************************************************/
 
 #ifndef STACKS_MAIN_H
 #define STACKS_MAIN_H
 
-// you probably need all these,
-// but if you don't get rid of what you don't need
+#include "stack.h"
 #include <stdlib.h>   /* srand, rand */
 #include <time.h>     /* time */
 #include <iostream>   /* cout, endl */
 #include <string>     /* string */
 #include "functions.h"
+#define MULTIPLIER 1.5 // used to test overflow and underflow
 
-/*
- * additional directives here
- * 
- */
-
+using std::cout;
+using std::endl;
 
 #endif /* STACKS_MAIN_H */
